e3909f4b356b923809dad844cb76ed7b6d94d5bd
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-03-07 09:04:53 +0300

    print and remove the duplicate in items

---
+++ duplicate_array.php
@@ -0,0 +1,21 @@
+<?php
+	
+	$a = array(1, 5, 2, 5, 1, 3, 2, 4, 5);
+	
+	
+	echo "Original Array : \n";
+	print_r($a);
+	
+	
+	$a = array_flip($a);
+
+
+	$a = array_flip($a);
+
+	
+	$a= array_values($a);
+
+	
+	echo "\nUpdated Array : \n ";
+	print_r($a);
+?>
